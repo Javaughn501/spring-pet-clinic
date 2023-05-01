41052ff6f32ab5d69f6b2472bda1ea2d89a3c5e1
Javaughn501 <javaughnstephenson@Javaughns-MacBook-Pro.local>
2023-05-01 09:49:11 -0500

    Implement interfaces for services. Closes #5

---
+++ spring-pet-clinic-data/src/main/java/udemy/spring/services/VetService.java
@@ -0,0 +1,11 @@
+package udemy.spring.services;
+
+import udemy.spring.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findall();
+}
