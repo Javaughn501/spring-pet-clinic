e43a3c133ffd41f3fd7b4cbd34633e0dd83d8e3d
Javaughn501 <javaughnstephenson@Javaughns-MacBook-Pro.local>
2023-05-01 12:25:35 -0500

    Added Vet Index Page and Controller. Closes #8

---
+++ spring-pet-clinic-web/src/main/java/udemy/spring/controllers/VetController.java
@@ -0,0 +1,15 @@
+package udemy.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets","vets/index"})
+    public String ListOfVets() {
+        return "vets/index";
+    }
+
+
+}
