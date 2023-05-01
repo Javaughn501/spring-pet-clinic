f961999172980a9cb2ca7c0c97d8c328107e78c1
Javaughn501 <javaughnstephenson@Javaughns-MacBook-Pro.local>
2023-05-01 11:23:49 -0500

    Refactoring to common interface. Closes #11

--- spring-pet-clinic-data/src/main/java/udemy/spring/services/PetService.java
+++ spring-pet-clinic-data/src/main/java/udemy/spring/services/PetService.java
@@ -4,8 +4,6 @@ import udemy.spring.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findall();
+public interface PetService extends CrudService<Pet, Long> {
+
 }
