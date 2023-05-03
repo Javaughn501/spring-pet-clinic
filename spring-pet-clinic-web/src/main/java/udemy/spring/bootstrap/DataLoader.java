9e586fe65cb85903d9012e6c2bb2a2f492e4c91e
Javaughn501 <javaughnstephenson@digijmres-2-123-160-69.digicelbroadband.com>
2023-05-03 15:32:23 -0500

    Added pet type in startup. Closes #32

--- spring-pet-clinic-web/src/main/java/udemy/spring/bootstrap/DataLoader.java
+++ spring-pet-clinic-web/src/main/java/udemy/spring/bootstrap/DataLoader.java
@@ -3,25 +3,36 @@ package udemy.spring.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import udemy.spring.model.Owner;
+import udemy.spring.model.PetType;
 import udemy.spring.model.Vet;
 import udemy.spring.services.OwnerService;
+import udemy.spring.services.PetTypeService;
 import udemy.spring.services.VetService;
-import udemy.spring.services.map.OwnerServiceMap;
-import udemy.spring.services.map.VetServiceMap;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Micheal");
