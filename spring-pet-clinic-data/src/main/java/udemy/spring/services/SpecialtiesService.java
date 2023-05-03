ea5d04bfe55f8828bf9d27ba11c77f1380bbb8e3
Javaughn501 <javaughnstephenson@Javaughns-MacBook-Pro.local>
2023-05-03 15:54:46 -0500

    Added specialty map service. Closes #31

---
+++ spring-pet-clinic-data/src/main/java/udemy/spring/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package udemy.spring.services;
+
+import udemy.spring.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
---
+++ spring-pet-clinic-data/src/main/java/udemy/spring/services/map/SpecialtiesServiceMapService.java
@@ -0,0 +1,35 @@
+package udemy.spring.services.map;
+
+import org.springframework.stereotype.Service;
+import udemy.spring.model.Specialty;
+import udemy.spring.services.SpecialtiesService;
+
+import java.util.Set;
+
+@Service
+public class SpecialtiesServiceMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.deleteByObject(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
