bf83b364a6e2daa624d79f61aec2ebff21eb9be1
Javaughn501 <javaughnstephenson@Javaughns-MacBook-Pro.local>
2023-05-04 14:37:14 -0500

    Implemented visit map service. Closes #46

---
+++ spring-pet-clinic-data/src/main/java/udemy/spring/services/VisitService.java
@@ -0,0 +1,6 @@
+package udemy.spring.services;
+
+import udemy.spring.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ spring-pet-clinic-data/src/main/java/udemy/spring/services/map/VisitServiceMapImpl.java
@@ -0,0 +1,38 @@
+package udemy.spring.services.map;
+
+import udemy.spring.model.Visit;
+import udemy.spring.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMapImpl extends AbstractMapService<Visit, Long> implements VisitService  {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+
+        if (object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.deleteByObject(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
