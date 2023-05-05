6c61dc4579875ed3c8de51503ca2f9b54d7105a7
Javaughn501 <javaughnstephenson@Javaughns-MacBook-Pro.local>
2023-05-05 09:38:58 -0500

    Added project Lombok. Closes #51

--- spring-pet-clinic-data/src/main/java/udemy/spring/model/Vet.java
+++ spring-pet-clinic-data/src/main/java/udemy/spring/model/Vet.java
@@ -1,10 +1,16 @@
 package udemy.spring.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -16,11 +22,4 @@ public class Vet extends Person {
     )
     private Set<Specialty> specialties = new HashSet<>();
 
-    public Set<Specialty> getSpecialties() {
-        return specialties;
-    }
-
-    public void setSpecialties(Set<Specialty> specialties) {
-        this.specialties = specialties;
-    }
 }
